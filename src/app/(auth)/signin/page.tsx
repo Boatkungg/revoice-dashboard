5b5c7a271a34bd52b197765f43aab640916746bf
B_K <chayanongg2550@gmail.com>
2025-07-01 02:16:27 +0700

    feat: add UI components for card, form, input, label, separator, sheet, sidebar, skeleton, tooltip, and mobile detection hook
    - Implemented Card component with subcomponents: CardHeader, CardTitle, CardDescription, CardAction, CardContent, CardFooter.
    - Created Form component with context and form field management using react-hook-form.
    - Developed Input component with styling and accessibility features.
    - Added Label component for form labeling.
    - Introduced Separator component for visual separation in layouts.
    - Built Sheet component for modal-like functionality with header, footer, title, and description.
    - Established Sidebar component with context for state management and mobile responsiveness.
    - Created Skeleton component for loading states.
    - Implemented Tooltip component for providing additional information on hover.
    - Added useIsMobile hook for detecting mobile device state.
    - Included utility function for class name merging.
    - Added middleware for redirecting to sign-in page on root access.

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Eye, EyeClosed } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(1, "Password is required")
+})
+
+export default function SignInPage() {
+    const [passvisibility, setPassVisibility] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>
+                    <div className=" text-2xl font-semibold">Sign in</div>
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="user@example.com" type="email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <div className="w-full flex items-center space-x-1">
+                                            <Input placeholder="········" type={passvisibility ? "text" : "password"} {...field} />
+                                            <Button variant="outline" type="button" size="icon" onClick={() => setPassVisibility(!passvisibility)}>
+                                                {
+                                                    passvisibility ? (
+                                                        <EyeClosed className="h-4 w-4"/>
+                                                    ) : (
+                                                        <Eye className="h-4 w-4"/>
+                                                    )
+                                                }
+                                            </Button>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button type="submit" className=" w-full mt-4">Sign in</Button>
+                        <div className="text-sm">
+                            Don&apos;t have an account? <Link href="/signup" className="underline">Sign up</Link>
+                        </div>
+                    </form>
+                </Form>
+            </CardContent>
+            {/* <div className="px-6">
+                <Separator/>
+            </div>
+            <CardFooter>
+                    
+            </CardFooter> */}
+        </Card>
+    );
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Eye, EyeClosed } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+    username: z.string().min(1, "Username is required"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(1, "Password is required"),
+    confirmPassword: z.string().min(1, "Confirm Password is required"),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+})
+
+export default function SignUpPage() {
+    const [passvisibility, setPassVisibility] = useState(false);
+    const [confirmPassvisibility, setConfirmPassVisibility] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        }
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>
+                    <div className=" text-2xl font-semibold">Sign up</div>
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="ex. JoneDoe123" type="text" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="user@example.com" type="email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <div className="w-full flex items-center space-x-1">
+                                            <Input placeholder="········" type={passvisibility ? "text" : "password"} {...field} />
+                                            <Button variant="outline" type="button" size="icon" onClick={() => setPassVisibility(!passvisibility)}>
+                                                {
+                                                    passvisibility ? (
+                                                        <EyeClosed className="h-4 w-4"/>
+                                                    ) : (
+                                                        <Eye className="h-4 w-4"/>
+                                                    )
+                                                }
+                                            </Button>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="confirmPassword"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Confirm password</FormLabel>
+                                    <FormControl>
+                                        <div className="w-full flex items-center space-x-1">
+                                            <Input placeholder="········" type={confirmPassvisibility ? "text" : "password"} {...field} />
+                                            <Button variant="outline" type="button" size="icon" onClick={() => setConfirmPassVisibility(!confirmPassvisibility)}>
+                                                {
+                                                    confirmPassvisibility ? (
+                                                        <EyeClosed className="h-4 w-4"/>
+                                                    ) : (
+                                                        <Eye className="h-4 w-4"/>
+                                                    )
+                                                }
+                                            </Button>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button type="submit" className=" w-full mt-4">Sign up</Button>
+                        <div className="text-sm">
+                            Already have an account? <Link href="/signin" className="underline">Sign in</Link>
+                        </div>
+                    </form>
+                </Form>
+            </CardContent>
+            {/* <div className="px-6">
+                <Separator/>
+            </div>
+            <CardFooter>
+                    
+            </CardFooter> */}
+        </Card>
+    );
+}
